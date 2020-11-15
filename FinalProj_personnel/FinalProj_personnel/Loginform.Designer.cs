2b69e2619ae3b11a442b259054160524dc38a08c
김지현 <jihyeon1542@gmail.com>
2020-11-15 21:35:39 +0900

    수정

--- FinalProj_personnel/FinalProj_personnel/Loginform.Designer.cs
+++ FinalProj_personnel/FinalProj_personnel/Loginform.Designer.cs
@@ -40,9 +40,10 @@
             // 
             // button_login
             // 
-            this.button_login.Location = new System.Drawing.Point(194, 51);
+            this.button_login.Location = new System.Drawing.Point(277, 76);
+            this.button_login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_login.Name = "button_login";
-            this.button_login.Size = new System.Drawing.Size(75, 23);
+            this.button_login.Size = new System.Drawing.Size(109, 53);
             this.button_login.TabIndex = 0;
             this.button_login.Text = "Log_IN";
             this.button_login.UseVisualStyleBackColor = true;
@@ -50,46 +51,53 @@
             // 
             // textBox_ID
             // 
-            this.textBox_ID.Location = new System.Drawing.Point(57, 33);
+            this.textBox_ID.Location = new System.Drawing.Point(81, 50);
+            this.textBox_ID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_ID.Name = "textBox_ID";
-            this.textBox_ID.Size = new System.Drawing.Size(100, 21);
+            this.textBox_ID.Size = new System.Drawing.Size(141, 28);
             this.textBox_ID.TabIndex = 1;
             // 
             // textBox_pw
             // 
-            this.textBox_pw.Location = new System.Drawing.Point(57, 71);
+            this.textBox_pw.Location = new System.Drawing.Point(81, 106);
+            this.textBox_pw.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_pw.Name = "textBox_pw";
-            this.textBox_pw.Size = new System.Drawing.Size(100, 21);
+            this.textBox_pw.Size = new System.Drawing.Size(141, 28);
             this.textBox_pw.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Location = new System.Drawing.Point(9, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.Size = new System.Drawing.Size(22, 18);
             this.label1.TabIndex = 3;
             this.label1.Text = "ID";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 74);
+            this.label2.Location = new System.Drawing.Point(9, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.Size = new System.Drawing.Size(34, 18);
             this.label2.TabIndex = 4;
             this.label2.Text = "PW";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.checkBox1);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.button_login);
             this.groupBox1.Controls.Add(this.textBox_pw);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.textBox_ID);
-            this.groupBox1.Location = new System.Drawing.Point(59, 33);
+            this.groupBox1.Location = new System.Drawing.Point(58, 50);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(374, 333);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(407, 213);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "login";
@@ -97,27 +105,27 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(513, 70);
+            this.checkBox1.Location = new System.Drawing.Point(131, 171);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(86, 16);
+            this.checkBox1.Size = new System.Drawing.Size(106, 22);
             this.checkBox1.TabIndex = 6;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "자동입력";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Loginform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(512, 309);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Loginform";
-            this.Text = "Form2";
+            this.Text = "로그인 화면";
             this.Load += new System.EventHandler(this.Loginform_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
