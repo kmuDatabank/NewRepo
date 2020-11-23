80dd15f055c781b436d16a6302629723b8b609fa
rudcks5562 <72645618+rudcks5562@users.noreply.github.com>
2020-11-24 00:34:44 +0900

    출근부

--- FinalProj_personnel/FinalProj_personnel/FormTimeCardPesonnel.Designer.cs
+++ FinalProj_personnel/FinalProj_personnel/FormTimeCardPesonnel.Designer.cs
@@ -54,9 +54,9 @@
             this.groupBox1.Controls.Add(this.dataGridViewGotoWork);
             this.groupBox1.Controls.Add(this.dateTimePicker1);
             this.groupBox1.Location = new System.Drawing.Point(270, 25);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox1.Size = new System.Drawing.Size(328, 320);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
@@ -65,18 +65,19 @@
             // buttonGotoWorkShow
             // 
             this.buttonGotoWorkShow.Location = new System.Drawing.Point(256, 33);
-            this.buttonGotoWorkShow.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonGotoWorkShow.Margin = new System.Windows.Forms.Padding(2);
             this.buttonGotoWorkShow.Name = "buttonGotoWorkShow";
             this.buttonGotoWorkShow.Size = new System.Drawing.Size(55, 19);
             this.buttonGotoWorkShow.TabIndex = 3;
             this.buttonGotoWorkShow.Text = "보기";
             this.buttonGotoWorkShow.UseVisualStyleBackColor = true;
+            this.buttonGotoWorkShow.Click += new System.EventHandler(this.buttonGotoWorkShow_Click);
             // 
             // dataGridViewGotoWork
             // 
             this.dataGridViewGotoWork.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewGotoWork.Location = new System.Drawing.Point(4, 60);
-            this.dataGridViewGotoWork.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridViewGotoWork.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridViewGotoWork.Name = "dataGridViewGotoWork";
             this.dataGridViewGotoWork.RowHeadersWidth = 62;
             this.dataGridViewGotoWork.RowTemplate.Height = 30;
@@ -85,10 +86,10 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd-tt hh:mm";
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd-tt HH:mm";
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker1.Location = new System.Drawing.Point(35, 31);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
             this.dateTimePicker1.TabIndex = 0;
@@ -96,12 +97,13 @@
             // buttonStartMemory
             // 
             this.buttonStartMemory.Location = new System.Drawing.Point(197, 45);
-            this.buttonStartMemory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonStartMemory.Margin = new System.Windows.Forms.Padding(2);
             this.buttonStartMemory.Name = "buttonStartMemory";
             this.buttonStartMemory.Size = new System.Drawing.Size(41, 21);
             this.buttonStartMemory.TabIndex = 2;
             this.buttonStartMemory.Text = "기록";
             this.buttonStartMemory.UseVisualStyleBackColor = true;
+            this.buttonStartMemory.Click += new System.EventHandler(this.buttonStartMemory_Click);
             // 
             // groupBox2
             // 
@@ -110,9 +112,9 @@
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.buttonStartMemory);
             this.groupBox2.Location = new System.Drawing.Point(11, 37);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox2.Size = new System.Drawing.Size(245, 125);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
@@ -121,7 +123,7 @@
             // buttonStartThis
             // 
             this.buttonStartThis.Location = new System.Drawing.Point(69, 87);
-            this.buttonStartThis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonStartThis.Margin = new System.Windows.Forms.Padding(2);
             this.buttonStartThis.Name = "buttonStartThis";
             this.buttonStartThis.Size = new System.Drawing.Size(119, 23);
             this.buttonStartThis.TabIndex = 5;
@@ -132,7 +134,7 @@
             // textBoxStartTime
             // 
             this.textBoxStartTime.Location = new System.Drawing.Point(77, 45);
-            this.textBoxStartTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxStartTime.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxStartTime.Name = "textBoxStartTime";
             this.textBoxStartTime.Size = new System.Drawing.Size(112, 21);
             this.textBoxStartTime.TabIndex = 4;
@@ -154,9 +156,9 @@
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Controls.Add(this.buttonEndMemory);
             this.groupBox3.Location = new System.Drawing.Point(11, 186);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox3.Size = new System.Drawing.Size(245, 126);
             this.groupBox3.TabIndex = 4;
             this.groupBox3.TabStop = false;
@@ -165,7 +167,7 @@
             // buttonEndThis
             // 
             this.buttonEndThis.Location = new System.Drawing.Point(69, 93);
-            this.buttonEndThis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonEndThis.Margin = new System.Windows.Forms.Padding(2);
             this.buttonEndThis.Name = "buttonEndThis";
             this.buttonEndThis.Size = new System.Drawing.Size(119, 23);
             this.buttonEndThis.TabIndex = 7;
@@ -176,7 +178,7 @@
             // textBoxEndTime
             // 
             this.textBoxEndTime.Location = new System.Drawing.Point(77, 51);
-            this.textBoxEndTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxEndTime.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxEndTime.Name = "textBoxEndTime";
             this.textBoxEndTime.Size = new System.Drawing.Size(112, 21);
             this.textBoxEndTime.TabIndex = 6;
@@ -194,12 +196,13 @@
             // buttonEndMemory
             // 
             this.buttonEndMemory.Location = new System.Drawing.Point(196, 51);
-            this.buttonEndMemory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonEndMemory.Margin = new System.Windows.Forms.Padding(2);
             this.buttonEndMemory.Name = "buttonEndMemory";
             this.buttonEndMemory.Size = new System.Drawing.Size(42, 21);
             this.buttonEndMemory.TabIndex = 2;
             this.buttonEndMemory.Text = "기록";
             this.buttonEndMemory.UseVisualStyleBackColor = true;
+            this.buttonEndMemory.Click += new System.EventHandler(this.buttonEndMemory_Click);
             // 
             // FormTimeCardPesonnel
             // 
@@ -209,7 +212,7 @@
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormTimeCardPesonnel";
             this.Text = "출근부 화면";
             this.groupBox1.ResumeLayout(false);
