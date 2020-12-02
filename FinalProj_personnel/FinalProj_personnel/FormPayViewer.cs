f56335cd8158d79e70d0dc8e3ef143418968c059
5293442@stu.kmu.ac.kr <5293442@stu.kmu.ac.kr>
2020-12-03 02:01:36 +0900

    공제, 최종임금

--- FinalProj_personnel/FinalProj_personnel/FormPayViewer.cs
+++ FinalProj_personnel/FinalProj_personnel/FormPayViewer.cs
@@ -73,13 +73,64 @@ namespace FinalProj_personnel
         //공제
         private void buttonDeduction_Click(object sender, EventArgs e)
         {
-
+            int per_n = DBM.GetDBMinstance().personnel_n();
+            string[] per_info = DBM.GetDBMinstance().personnelinfo(per_n);
+            for (int i = 0; i < per_n; i++)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = per_info[i];
+                DateTime dt = dateTimePicker1.Value;
+                string str1 = Convert.ToString(dt.Year);
+                string str2 = Convert.ToString(dt.Month);
+                string str = str1 + "-" + str2 + "-";
+                //이름 , 월 로 검색-> 출근부에서 기본급 전부 더함
+                int nor = (DBM.GetDBMinstance().pay_normal(per_info[i], str)) * 10000;
+                int plus = (DBM.GetDBMinstance().pay_plus(per_info[i], str)) * 5000;
+                int pay = nor + plus;
+                int a = 0; // 연금 보험료 9%
+                a = pay * 9 / 100;
+                int b = 0; // 고용 보험료 1.85%
+                b = pay * 185 / 10000;
+                int c = 0; // 국민건강보험(건강보험) 6.67%
+                c = pay * 667 / 10000;
+                int d = 0; // 국민겅강보험 (장기요양보험) 건강보험의 10.25%
+                d = c * 1025 / 10000;
+                int deduction = a + b + c + d;
+                item.SubItems.Add(Convert.ToString(deduction));
+                listViewDeduction.Items.Add(item);
+            }
         }
 
         //최종 임금
         private void buttonFinalWage_Click(object sender, EventArgs e)
         {
-
+            int per_n = DBM.GetDBMinstance().personnel_n();
+            string[] per_info = DBM.GetDBMinstance().personnelinfo(per_n);
+            for (int i = 0; i < per_n; i++)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = per_info[i];
+                DateTime dt = dateTimePicker1.Value;
+                string str1 = Convert.ToString(dt.Year);
+                string str2 = Convert.ToString(dt.Month);
+                string str = str1 + "-" + str2 + "-";
+                //이름 , 월 로 검색-> 출근부에서 기본급 전부 더함
+                int nor = (DBM.GetDBMinstance().pay_normal(per_info[i], str)) * 10000;
+                int plus = (DBM.GetDBMinstance().pay_plus(per_info[i], str)) * 5000;
+                int pay = nor + plus;
+                int a = 0; // 연금 보험료 9%
+                a = pay * 9 / 100;
+                int b = 0; // 고용 보험료 1.85%
+                b = pay * 185 / 10000;
+                int c = 0; // 국민건강보험(건강보험) 6.67%
+                c = pay * 667 / 10000;
+                int d = 0; // 국민겅강보험 (장기요양보험) 건강보험의 10.25%
+                d = c * 1025 / 10000;
+                int deduction = a + b + c + d;
+                int wage = pay - deduction;
+                item.SubItems.Add(Convert.ToString(wage));
+                listViewFinalWage.Items.Add(item);
+            }
         }
 
       
