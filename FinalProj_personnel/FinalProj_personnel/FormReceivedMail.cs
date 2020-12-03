70db44eae25bf4f3d290e73838c1b619caaa4f87
kms <kms31024@gmail.com>
2020-12-03 15:37:20 +0900

    SendMail의 받을 사람 콤보박스 값 추가 진행 중

--- FinalProj_personnel/FinalProj_personnel/FormReceivedMail.cs
+++ FinalProj_personnel/FinalProj_personnel/FormReceivedMail.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,66 @@ namespace FinalProj_personnel
 {
     public partial class FormReceivedMail : Form
     {
+        string strConn = "Server=49.50.174.201;Database=databank;Uid=databank;Pwd=dbdb;Charset=utf8";
+
+        string name = "";
+        FormReadReceived mail;
         public FormReceivedMail()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public FormReceivedMail(string name, FormReadReceived form)
+        {
+            InitializeComponent();
+            this.name = name;
+            mail = form;
+
+            string strqry = "select * from databank.RMails where not WHO=" + name;
+
+            using (MySqlConnection conn = new MySqlConnection(strConn))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(strqry, conn);
+                cmd.CommandType = CommandType.Text;
+                MySqlDataReader R = cmd.ExecuteReader();
+
+                if (R.HasRows)
+                {
+                    while (R.Read())
+                    {
+                        ListViewItem lvt = new ListViewItem();
+                        lvt.SubItems.Add(R.GetString(0));
+                        lvt.SubItems.Add(R.GetString(1));
+                        lvt.SubItems.Add(R.GetString(2));
+                        lvt.SubItems.Add(R.GetString(3));
+                        ListViewReceived.Items.Add(lvt);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("데이터가 없습니다");
+                }
+            }
+
+        }
+
+        private void ButtonOpenMail_Click(object sender, EventArgs e)
         {
             FormReadReceived form = new FormReadReceived();
+            if (ListViewReceived.SelectedItems.Count != 0)
+            {
+                int SelectRow = ListViewReceived.SelectedItems[0].Index;
+
+                string who = ListViewReceived.Items[SelectRow].SubItems[0].Text;
+                string title = ListViewReceived.Items[SelectRow].SubItems[1].Text;
+                string content = ListViewReceived.Items[SelectRow].SubItems[2].Text;
+
+                mail.TextBoxWho.Text = who;
+                mail.TextBoxTitle.Text = title;
+                mail.TextBoxContent.Text = content;
+            }
+
             form.Show();
         }
     }
