42e6e4c2a6b1c9e8aa1fdb7bd81a92251230e31f
김지현 <jihyeon1542@gmail.com>
2020-12-06 17:41:30 +0900

    폼 수정

--- FinalProj_personnel/FinalProj_personnel/FormPayViewer.Designer.cs
+++ FinalProj_personnel/FinalProj_personnel/FormPayViewer.Designer.cs
@@ -53,10 +53,9 @@
             // 
             // buttonMoney
             // 
-            this.buttonMoney.Location = new System.Drawing.Point(43, 70);
-            this.buttonMoney.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonMoney.Location = new System.Drawing.Point(61, 105);
             this.buttonMoney.Name = "buttonMoney";
-            this.buttonMoney.Size = new System.Drawing.Size(100, 39);
+            this.buttonMoney.Size = new System.Drawing.Size(143, 58);
             this.buttonMoney.TabIndex = 6;
             this.buttonMoney.Text = "기본급";
             this.buttonMoney.UseVisualStyleBackColor = true;
@@ -65,19 +64,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(179, 83);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(256, 124);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.Size = new System.Drawing.Size(17, 18);
             this.label1.TabIndex = 7;
             this.label1.Text = "+";
             // 
             // buttonAllowance
             // 
-            this.buttonAllowance.Location = new System.Drawing.Point(231, 70);
-            this.buttonAllowance.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonAllowance.Location = new System.Drawing.Point(330, 105);
             this.buttonAllowance.Name = "buttonAllowance";
-            this.buttonAllowance.Size = new System.Drawing.Size(100, 39);
+            this.buttonAllowance.Size = new System.Drawing.Size(143, 58);
             this.buttonAllowance.TabIndex = 8;
             this.buttonAllowance.Text = "수당";
             this.buttonAllowance.UseVisualStyleBackColor = true;
@@ -86,19 +83,17 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(364, 83);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(520, 124);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.Size = new System.Drawing.Size(17, 18);
             this.label2.TabIndex = 9;
             this.label2.Text = "-";
             // 
             // buttonDeduction
             // 
-            this.buttonDeduction.Location = new System.Drawing.Point(418, 70);
-            this.buttonDeduction.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDeduction.Location = new System.Drawing.Point(597, 105);
             this.buttonDeduction.Name = "buttonDeduction";
-            this.buttonDeduction.Size = new System.Drawing.Size(100, 39);
+            this.buttonDeduction.Size = new System.Drawing.Size(143, 58);
             this.buttonDeduction.TabIndex = 10;
             this.buttonDeduction.Text = "공제";
             this.buttonDeduction.UseVisualStyleBackColor = true;
@@ -107,19 +102,17 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(553, 83);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(790, 124);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.Size = new System.Drawing.Size(17, 18);
             this.label3.TabIndex = 11;
             this.label3.Text = "=";
             // 
             // buttonFinalWage
             // 
-            this.buttonFinalWage.Location = new System.Drawing.Point(611, 70);
-            this.buttonFinalWage.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonFinalWage.Location = new System.Drawing.Point(873, 105);
             this.buttonFinalWage.Name = "buttonFinalWage";
-            this.buttonFinalWage.Size = new System.Drawing.Size(100, 39);
+            this.buttonFinalWage.Size = new System.Drawing.Size(143, 58);
             this.buttonFinalWage.TabIndex = 12;
             this.buttonFinalWage.Text = "최종 임금";
             this.buttonFinalWage.UseVisualStyleBackColor = true;
@@ -132,10 +125,9 @@
             this.columnHeaderBasePay});
             this.listViewMoney.GridLines = true;
             this.listViewMoney.HideSelection = false;
-            this.listViewMoney.Location = new System.Drawing.Point(22, 132);
-            this.listViewMoney.Margin = new System.Windows.Forms.Padding(2);
+            this.listViewMoney.Location = new System.Drawing.Point(31, 198);
             this.listViewMoney.Name = "listViewMoney";
-            this.listViewMoney.Size = new System.Drawing.Size(149, 263);
+            this.listViewMoney.Size = new System.Drawing.Size(211, 392);
             this.listViewMoney.TabIndex = 13;
             this.listViewMoney.UseCompatibleStateImageBehavior = false;
             this.listViewMoney.View = System.Windows.Forms.View.Details;
@@ -154,11 +146,11 @@
             this.listViewAllowance.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
+            this.listViewAllowance.GridLines = true;
             this.listViewAllowance.HideSelection = false;
-            this.listViewAllowance.Location = new System.Drawing.Point(211, 132);
-            this.listViewAllowance.Margin = new System.Windows.Forms.Padding(2);
+            this.listViewAllowance.Location = new System.Drawing.Point(301, 198);
             this.listViewAllowance.Name = "listViewAllowance";
-            this.listViewAllowance.Size = new System.Drawing.Size(149, 263);
+            this.listViewAllowance.Size = new System.Drawing.Size(211, 392);
             this.listViewAllowance.TabIndex = 14;
             this.listViewAllowance.UseCompatibleStateImageBehavior = false;
             this.listViewAllowance.View = System.Windows.Forms.View.Details;
@@ -179,10 +171,9 @@
             this.columnHeaderDeduction});
             this.listViewDeduction.GridLines = true;
             this.listViewDeduction.HideSelection = false;
-            this.listViewDeduction.Location = new System.Drawing.Point(399, 132);
-            this.listViewDeduction.Margin = new System.Windows.Forms.Padding(2);
+            this.listViewDeduction.Location = new System.Drawing.Point(570, 198);
             this.listViewDeduction.Name = "listViewDeduction";
-            this.listViewDeduction.Size = new System.Drawing.Size(149, 263);
+            this.listViewDeduction.Size = new System.Drawing.Size(211, 392);
             this.listViewDeduction.TabIndex = 15;
             this.listViewDeduction.UseCompatibleStateImageBehavior = false;
             this.listViewDeduction.View = System.Windows.Forms.View.Details;
@@ -203,10 +194,9 @@
             this.columnHeaderTotalPay});
             this.listViewFinalWage.GridLines = true;
             this.listViewFinalWage.HideSelection = false;
-            this.listViewFinalWage.Location = new System.Drawing.Point(588, 132);
-            this.listViewFinalWage.Margin = new System.Windows.Forms.Padding(2);
+            this.listViewFinalWage.Location = new System.Drawing.Point(840, 198);
             this.listViewFinalWage.Name = "listViewFinalWage";
-            this.listViewFinalWage.Size = new System.Drawing.Size(149, 263);
+            this.listViewFinalWage.Size = new System.Drawing.Size(211, 392);
             this.listViewFinalWage.TabIndex = 16;
             this.listViewFinalWage.UseCompatibleStateImageBehavior = false;
             this.listViewFinalWage.View = System.Windows.Forms.View.Details;
@@ -222,16 +212,18 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(22, 21);
+            this.dateTimePicker1.Location = new System.Drawing.Point(31, 32);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(93, 21);
+            this.dateTimePicker1.Size = new System.Drawing.Size(131, 28);
             this.dateTimePicker1.TabIndex = 19;
             // 
             // buttonpayfix
             // 
-            this.buttonpayfix.Location = new System.Drawing.Point(231, 415);
+            this.buttonpayfix.Location = new System.Drawing.Point(330, 622);
+            this.buttonpayfix.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonpayfix.Name = "buttonpayfix";
-            this.buttonpayfix.Size = new System.Drawing.Size(100, 23);
+            this.buttonpayfix.Size = new System.Drawing.Size(143, 34);
             this.buttonpayfix.TabIndex = 20;
             this.buttonpayfix.Text = "수당수정";
             this.buttonpayfix.UseVisualStyleBackColor = true;
@@ -239,9 +231,9 @@
             // 
             // FormPayViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(768, 450);
+            this.ClientSize = new System.Drawing.Size(1097, 675);
             this.Controls.Add(this.buttonpayfix);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.listViewFinalWage);
@@ -255,7 +247,6 @@
             this.Controls.Add(this.buttonAllowance);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonMoney);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormPayViewer";
             this.Text = "급여보기";
             this.ResumeLayout(false);
